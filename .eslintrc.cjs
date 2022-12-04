7b665ff892c44d91eacf4a7d1169c772dbea7abc
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2022-12-03 22:42:43 -0300

    Refeito projeto com nodejs

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:jsonc/recommended-with-json5',
+    'plugin:json/recommended'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
